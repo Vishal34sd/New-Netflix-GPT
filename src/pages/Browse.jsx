20012aa964d3ca92ee9edc1f41a5c45d50e29dad
vishal-07 <dubeyvishal714@gmail.com>
2025-07-31 12:18:16 +0530

    api fetch

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import Header from '../components/Header'
+import { API_OPTIONS, MOVIE_API } from '../utils/constant'
+import { useEffect } from 'react'
 
 const Browse = () => {
+
+    const nowPlayingMovies = async ()=>{
+        const res = await fetch(MOVIE_API,API_OPTIONS);
+        const json = await res.json();
+        console.log(json);
+    }
+
+    useEffect(()=>{
+        nowPlayingMovies()
+    },[])
   return (
     <div>
       <Header/>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,3 +5,4 @@ export const API_OPTIONS = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWM2ZGQzYjM2NTFjMjliOWE5NTRjNmM2YjhkZjA4YyIsIm5iZiI6MTc1Mzg4MzMwOS4wOTYsInN1YiI6IjY4OGEyMmFkMmFjNTFiYTg5NGQwMjQxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j1m1SqUFx20Bd4PrCQByQUIElTcf5I3FTGbFyyqeXyM'
   }
 };
+export const  MOVIE_API='https://api.themoviedb.org/3/movie/now_playing?page=1'
